914423bfb235018282e0dfd57c58bec8c52dcb33
Efradgalio <efradgamer@gmail.com>
2022-10-17 06:07:27 +0700

    completing the saving and load the model in WSL

--- Week-5/churn-flask-app/predict.py
+++ Week-5/churn-flask-app/predict.py
@@ -1,5 +1,25 @@
 import pickle
 
+customer = {
+    "gender": "female",
+    "seniorcitizen": 0,
+    "partner": "yes",
+    "dependents": "no",
+    "phoneservice": "no",
+    "multiplelines": "no_phone_service",
+    "internetservice": "dsl",
+    "onlinesecurity": "no",
+    "deviceprotection": "no",
+    "techsupport": "no",
+    "streamingtv": "no",
+    "streamingmovies": "no",
+    "contract": "month-to-month",
+    "paperlessbilling": "yes",
+    "paymentmethod": "electronic_check",
+    "tenure": 1,
+    "monthlycharges": 29.85,
+    "totalcharges": 29.85
+}
 
 # Load the Model
 model_file = f'model_C=1.0.bin'
@@ -9,3 +29,8 @@ with open(model_file, 'rb') as f_in:
 # we don't need to import scikit-learn, but we need scikit-learn installed in our system,
 # so it will know what model and dv means.
 
+X = dv.transform([customer])
+y_pred = model.predict_proba(X)[0,1]
+
+print('input', customer)
+print('churn probability', y_pred)
