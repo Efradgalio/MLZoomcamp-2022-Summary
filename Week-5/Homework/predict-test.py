7166e86567a18f596a65dec3dffc3a37ad5759f2
Efradgalio <efradgamer@gmail.com>
2022-10-20 08:55:30 +0700

    completing homework week 5

--- Week-5/Homework/predict-test.py
+++ Week-5/Homework/predict-test.py
@@ -1,7 +1,8 @@
 import requests
 
-url = "http://0.0.0.0:9696/predict"
-client = {"reports": 0, "share": 0.245, "expenditure": 3.438, "owner": "yes"}
+url = "http://localhost:9696/predict"
+# client = {"reports": 0, "share": 0.245, "expenditure": 3.438, "owner": "yes"}
+client6 = {"reports": 0, "share": 0.245, "expenditure": 3.438, "owner": "yes"}
 
-response = requests.post(url, json=client).json()
+response = requests.post(url, json=client6).json()
 print(response)
